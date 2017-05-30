3ac42e6f2b34281c8f93f019bc2f03c0c3b9176f
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-05-30 04:41:39 -0600

    changed index.js code to be more in line with what heroku expects

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const
     express = require('express'),
-    app = express(),
-    port = 3001
+    app = express()
 
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static('public'))  
 app.set('view engine', 'pug')
 app.get('/', (req, res) => res.render('index'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(app.get('port'), () => console.log(`Example app listening on port ${app.get('port')}!`))
